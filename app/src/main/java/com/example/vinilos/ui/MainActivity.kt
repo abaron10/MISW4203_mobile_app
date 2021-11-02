c44d251775a11b16c3b93e04e7c16ba9897c5e7d
wrravelo2021 <wr.ravelo@uniandes.edu.co>
2021-11-02 17:49:27 -0500

    Base
    Base

--- app/src/main/java/com/example/vinilos/ui/MainActivity.kt
+++ app/src/main/java/com/example/vinilos/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.vinilos
+package com.example.vinilos.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.vinilos.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
