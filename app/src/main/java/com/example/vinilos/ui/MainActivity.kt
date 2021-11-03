9a6575db290875e1bface722c8ac44da6dc3a713
William Ravelo M <drummerwilliam@gmail.com>
2021-11-02 23:48:43 -0500

    fix conflict

--- app/src/main/java/com/example/vinilos/ui/MainActivity.kt
+++ app/src/main/java/com/example/vinilos/ui/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.vinilos.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.example.vinilos.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), View.OnClickListener {
+    private lateinit var btn_sign_as_user: Button
+    private lateinit var btn_sign_as_collector: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        btn_sign_as_user = findViewById(R.id.btn_sign_as_user)
+        btn_sign_as_collector = findViewById(R.id.btn_sign_as_collector)
+
+        btn_sign_as_user.setOnClickListener(this)
+        btn_sign_as_collector.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        goToActivity(MenuActivity::class.java)
     }
 }
