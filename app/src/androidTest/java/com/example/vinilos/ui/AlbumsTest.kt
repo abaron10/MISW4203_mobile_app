6ef8fc5b9feb7367c8a2ee3c2d09d78e4525b3ec
Laura Natalia Bello Jiménez <laura.bello@rappi.com>
2021-11-07 03:01:10 -0500

    first e2e test

--- app/src/androidTest/java/com/example/vinilos/ui/AlbumsTest.kt
+++ app/src/androidTest/java/com/example/vinilos/ui/AlbumsTest.kt
@@ -3,6 +3,8 @@ package com.example.vinilos.ui
 import android.view.Gravity
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.swipeDown
+import androidx.test.espresso.action.ViewActions.swipeUp
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.contrib.DrawerActions
 import androidx.test.espresso.contrib.DrawerMatchers
@@ -36,7 +38,35 @@ class AlbumsTest {
         Espresso.onView(ViewMatchers.withId(R.id.nav_view)).perform(
             NavigationViewActions.navigateTo(R.id.nav_item_two)
         )
-        Thread.sleep(5000);
+        Thread.sleep(5000)
+    }
+
+    fun goToAlbumsViewAsCollector() {
+        Espresso.onView(ViewMatchers.withId(R.id.btn_sign_as_collector)).check(
+            ViewAssertions.matches(ViewMatchers.isDisplayed())
+        )
+        Espresso.onView(ViewMatchers.withId(R.id.btn_sign_as_collector)).perform(
+            ViewActions.click()
+        )
+        Espresso.onView(ViewMatchers.withId(R.id.drawer_layout)).check(
+            ViewAssertions.matches(DrawerMatchers.isClosed(Gravity.LEFT))
+        ).perform(
+            DrawerActions.open()
+        )
+        Espresso.onView(ViewMatchers.withId(R.id.nav_view)).perform(
+            NavigationViewActions.navigateTo(R.id.nav_item_two)
+        )
+        Thread.sleep(5000)
+    }
+
+    fun goToCreateAlbum(){
+        Espresso.onView(ViewMatchers.withId(R.id.floating_add_button)).check(
+            ViewAssertions.matches(ViewMatchers.isDisplayed())
+        )
+        Espresso.onView(ViewMatchers.withId(R.id.floating_add_button)).perform(
+            ViewActions.click()
+        )
+        Thread.sleep(1000)
     }
 
     @Test
@@ -114,5 +144,80 @@ class AlbumsTest {
         Espresso.onView(ViewMatchers.withId(R.id.no_albums_text)).check(
             ViewAssertions.matches(ViewMatchers.isDisplayed())
         )
+    }
+
+    @Test
+    fun test_create_album_input_error(){
+        goToAlbumsViewAsCollector()
+        goToCreateAlbum()
+
+        Espresso.onView(ViewMatchers.withId(R.id.scroll_create_album))
+            .perform(swipeUp())
+
+        Espresso.onView(ViewMatchers.withId(R.id.btn_add_album)).perform(
+            ViewActions.click()
+        )
+
+        Espresso.onView(ViewMatchers.withId(R.id.scroll_create_album))
+            .perform(swipeDown())
+
+        Espresso.onView(
+            CoreMatchers.allOf(
+                ViewMatchers.withId(R.id.album_name),
+                ViewMatchers.hasErrorText("Album name cannot be empty")
+            )
+        ).check(
+            ViewAssertions.matches(ViewMatchers.isDisplayed())
+        )
+
+        Espresso.onView(
+            CoreMatchers.allOf(
+                ViewMatchers.withId(R.id.cover_image_url),
+                ViewMatchers.hasErrorText("Cover image url cannot be empty")
+            )
+        ).check(
+            ViewAssertions.matches(ViewMatchers.isDisplayed())
+        )
+
+
+        Espresso.onView(
+            CoreMatchers.allOf(
+                ViewMatchers.withId(R.id.release_date),
+                ViewMatchers.hasErrorText("Date cannot be empty")
+            )
+        ).check(
+            ViewAssertions.matches(ViewMatchers.isDisplayed())
+        )
+
+        Espresso.onView(
+            CoreMatchers.allOf(
+                ViewMatchers.withId(R.id.album_description),
+                ViewMatchers.hasErrorText("Description cannot be empty")
+            )
+        ).check(
+            ViewAssertions.matches(ViewMatchers.isDisplayed())
+        )
+
+        Espresso.onView(ViewMatchers.withId(R.id.scroll_create_album))
+            .perform(swipeUp())
+
+        Espresso.onView(
+            CoreMatchers.allOf(
+                ViewMatchers.withId(R.id.genre),
+                ViewMatchers.hasErrorText("Genre was not selected")
+            )
+        ).check(
+            ViewAssertions.matches(ViewMatchers.isDisplayed())
+        )
+
+        Espresso.onView(
+            CoreMatchers.allOf(
+                ViewMatchers.withId(R.id.record_label),
+                ViewMatchers.hasErrorText("Record label was not selected")
+            )
+        ).check(
+            ViewAssertions.matches(ViewMatchers.isDisplayed())
+        )
+
     }
 }
