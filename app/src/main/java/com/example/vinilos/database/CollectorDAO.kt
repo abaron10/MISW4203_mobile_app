73e0e68784818e028772e678ed3d56de6552d153
Emilson D. Quintero P <e.quinterop@uniandes.edu.co>
2021-11-21 20:47:28 -0500

    ajuste objeto DAO collectors

--- app/src/main/java/com/example/vinilos/database/CollectorDAO.kt
+++ app/src/main/java/com/example/vinilos/database/CollectorDAO.kt
@@ -1,6 +1,18 @@
 package com.example.vinilos.database
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.vinilos.models.Collector
 
 @Dao
 interface CollectorDAO {
+    @Query("SELECT * FROM collectors_table ORDER BY createdAt ASC")
+    fun getCollectors(): List<Collector>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(collector: Collector)
+
+    @Query("DELETE FROM collectors_table")
+    suspend fun deleteAll(): Int
 }
